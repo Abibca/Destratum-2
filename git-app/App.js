41f48033a8ff2c7ff8eef3ddd67e298e7ef6070e
Abibca <abinayaanju2507@gmail.com>
2023-06-16 11:54:06 +0530

    first

---
+++ git-app/App.js
@@ -0,0 +1,12 @@
+import {  View } from 'react-native';
+import ParentComponent from './component/screen1';
+
+
+export default function App() {
+  return (
+    <View style={{flex:1}}>
+    <ParentComponent/>
+    </View>
+  );
+}
+
---
+++ git-app/component/screen1.js
@@ -0,0 +1,117 @@
+import { View, Text, Button, TextInput, StyleSheet, TouchableOpacity, Image, ScrollView, Alert } from "react-native";
+import { useState } from "react";
+
+
+const ParentComponent = () => {
+    const [name, setname] = useState('');
+    const ChangeName = (e) => {
+        setname(e)
+
+    }
+
+    const [address, setAddress] = useState('');
+
+    const ChangeAddress = (e) => {
+        setAddress(e)
+    }
+
+    const [phone, setPhoneNo] = useState('');
+
+    const ChangePhone = (e) => {
+        setPhoneNo(e)
+    }
+
+    return (
+        <ScrollView>
+            <View style={{ flexDirection: "row", margin: 25, justifyContent: "space-around",marginTop:100 }}>
+                <Text>Name</Text>
+                <TextInput
+                    style={{
+                        borderWidth: 1,
+                        width: 200,
+                        height: 50,
+                        borderWidth: 2,
+                        borderRadius: 5,
+                        backgroundColor: "#FFF8DC",
+                    }}
+
+                    onChangeText={ChangeName}
+                    value={name}
+                    keyboardType="default"
+                /></View>
+            <View style={{ flexDirection: "row", margin: 25, justifyContent: "space-around" }}>
+                <Text>Address</Text>
+                <TextInput
+                    style={{
+                        borderWidth: 1,
+                        width: 200,
+                        height: 50,
+                        borderWidth: 2,
+                        borderRadius: 5,
+                        backgroundColor: "#FFF8DC",
+                    }}
+                    onChangeText={ChangeAddress}
+                    value={address}
+                    multiline={true}
+                /></View>
+            <View style={{ flexDirection: "row", margin: 25, justifyContent: "space-around" }}>
+                <Text>PhoneNo</Text>
+                <TextInput
+                    style={{
+                        borderWidth: 1,
+                        width: 200,
+                        height: 50,
+                        borderWidth: 2,
+                        borderRadius: 5,
+                        backgroundColor: "#FFF8DC",
+                    }}
+                    onChangeText={ChangePhone}
+                    value={phone}
+                    keyboardType="numeric"
+                /></View>
+
+            <TouchableOpacity
+                style={styles.button}
+            >
+                <Text style={{ padding: 8 }}>Press Me</Text>
+            </TouchableOpacity>
+        </ScrollView>
+    )
+
+
+}
+const styles = StyleSheet.create({
+    header: {
+        flexDirection: "row",
+        padding: 15,
+        width: "100%",
+        height: 80,
+        borderWidth: 2,
+        backgroundColor: "#C0C0C0"
+
+    },
+    input: {
+        width: 200,
+        height: 50,
+        borderWidth: 2,
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 5,
+        backgroundColor: "#FFF8DC",
+        padding: 10
+
+    },
+    button: {
+        width: 100,
+        height: 40,
+        borderColor: "#FF00FF",
+        borderWidth: 3,
+        borderRadius: 10,
+        backgroundColor: "#DDA0DD",
+        marginLeft: 150,
+        marginTop: 80,
+    }
+
+})
+
+export default ParentComponent;
